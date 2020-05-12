42b6ec44a722d61c0ec253257e7fa7173e4c3da7
yunwhite <s2018s12@e-mirim.hs.kr>
2020-05-12 22:58:20 +0900

    가장 큰 수, 가장 작은 수

--- showmeminmaxnumber/Program.cs
+++ showmeminmaxnumber/Program.cs
@@ -10,7 +10,21 @@ namespace showmeminmaxnumber
     {
         static void Main(string[] args)
         {
-            
+            int min = int.MaxValue;
+            int max = int.MinValue;
+            int n;
+            for (int i = 0; i < 5; i++)
+            {
+                Console.Write("숫자를 입력해주세요: ");
+                n = int.Parse(Console.ReadLine());
+                if (n < min)
+                    min = n;
+                if (n > max)
+                    max = n;
+            }
+            Console.WriteLine("--------------------");
+            Console.WriteLine("가장 큰 수: " + max);
+            Console.WriteLine("가장 작은 수: " + min);
         }
     }
 }
